77ddb6f3e21f71f060cdeb45b178a68f87f680e8
RoiFuries <roi.furies@gmail.com>
2020-07-17 11:16:03 -0500

    le agregue el reto de la clase 2

---
+++ index.js
@@ -0,0 +1,9 @@
+function hola() {
+    var card = document.getElementById(containerBoss);
+    if(containerBoss.style.display === "block"){
+        containerBoss.style.display = "none"
+    }else{
+        containerBoss.style.display = "block"
+    }
+
+}
